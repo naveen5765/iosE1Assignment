9acdf289ed62836bf80a5c690b7fe15974ac8736
Naveen Anandhan <antiquenaveen@gmail.com>
2024-02-26 08:26:40 +0100

    Initial Commit

---
+++ E1Assignment/E1AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  E1AssignmentApp.swift
+//  E1Assignment
+//
+//  Created by Naveen Anandhan on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct E1AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
